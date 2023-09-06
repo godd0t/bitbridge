c5ea0bbd2778daa9913118fbeb6f3bab52046853
godd0t <lirrishala@gmail.com>
2023-09-06 11:38:51 +0200

    fix: abandon transaction tests

--- bitbridge/__init__.py
+++ bitbridge/__init__.py
@@ -1,6 +1,6 @@
 from bitbridge.facade import BitBridgeFacade, AsyncBitBridgeFacade
 from bitbridge.rpc import BitBridgeConfig
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 __all__ = ["BitBridgeFacade", "AsyncBitBridgeFacade", "BitBridgeConfig"]

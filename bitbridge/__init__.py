d251b93296caab3733fb7597e6ae6edfee985732
godd0t <lirrishala@gmail.com>
2023-09-06 10:58:48 +0200

    fix: wallet file verification

--- bitbridge/__init__.py
+++ bitbridge/__init__.py
@@ -1,6 +1,6 @@
 from bitbridge.facade import BitBridgeFacade, AsyncBitBridgeFacade
 from bitbridge.rpc import BitBridgeConfig
 
-__version__ = "0.0.5"
+__version__ = "0.3.1"
 
 __all__ = ["BitBridgeFacade", "AsyncBitBridgeFacade", "BitBridgeConfig"]
